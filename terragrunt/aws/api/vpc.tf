74db6b766935fa402fae4e2a557b16f5911b0743
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-07-30 12:55:50 -0400

    fix: typo

--- terragrunt/aws/api/vpc.tf
+++ terragrunt/aws/api/vpc.tf
@@ -2,6 +2,6 @@ module "vpc" {
   source            = "github.com/cds-snc/terraform-modules?ref=v0.0.5//vpc"
   name              = var.product_name
   billing_tag_value = var.cost_center_code
-  high_availaiblity = true
+  high_availability = true
   enable_flow_log   = false
 }
