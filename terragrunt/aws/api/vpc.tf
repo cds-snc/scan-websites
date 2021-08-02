878bc2bc4a7c53d8802627870339bfd739c28dcb
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-08-01 23:47:21 -0400

    fix: fix issues with rds and default nacl
    Adds a nacl so the default nacl doesn't keep acquiring the subnets
    Adds an engine version to instance which should fix issue of not being
    able to find postgresql error

--- terragrunt/aws/api/vpc.tf
+++ terragrunt/aws/api/vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source            = "github.com/cds-snc/terraform-modules?ref=v0.0.5//vpc"
+  source            = "github.com/cds-snc/terraform-modules?ref=v0.0.11//vpc"
   name              = var.product_name
   billing_tag_value = var.billing_code
   high_availability = true
