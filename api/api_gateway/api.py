a1588e46d66a3ee46f341f426ea3825be27604be
Calvin Rodo <calvin.rodo@cds-snc.ca>
2021-08-03 19:02:16 -0400

    feat: add a healthcheck endpoint (#29)
    * feat: add a healthcheck endpoint that returns the db migration version
    
    - Refactor database outside of main.py
    - Refactor out logger

--- api/api_gateway/api.py
+++ api/api_gateway/api.py
@@ -1,5 +1,9 @@
 from os import environ
 from fastapi import FastAPI
+from sqlalchemy.sql import text
+from sqlalchemy.exc import SQLAlchemyError
+from database.db import db_session
+from logger import log
 
 app = FastAPI()
 
@@ -7,3 +11,23 @@ app = FastAPI()
 @app.get("/version")
 async def version():
     return {"version": environ.get("GIT_SHA", "unknown")}
+
+
+def get_db_version(sessionmaker):
+    session = sessionmaker()
+
+    query = "SELECT version_num FROM alembic_version"
+    full_name = session.execute(query).fetchone()[0]
+    return full_name
+
+
+@app.get("/healthcheck")
+async def healthcheck():
+    try:
+        full_name = get_db_version(db_session)
+        db_status = {"able_to_connect": True, "db_version": full_name}
+    except SQLAlchemyError as err:
+        log.error(err)
+        db_status = {"able_to_connect": False}
+
+    return {"database": db_status}
--- api/tests/api_gateway/test_api.py
+++ api/tests/api_gateway/test_api.py
@@ -2,9 +2,12 @@ import pytest
 import os
 
 from fastapi.testclient import TestClient
-from unittest.mock import patch
+from unittest.mock import patch, Mock
+
+import sqlalchemy
 
 from api_gateway import api
+from sqlalchemy.exc import SQLAlchemyError
 
 client = TestClient(api.app)
 
@@ -20,3 +23,23 @@ def test_version_with_GIT_SHA():
     response = client.get("/version")
     assert response.status_code == 200
     assert response.json() == {"version": "foo"}
+
+
+@patch("api_gateway.api.get_db_version")
+def test_healthcheck_success(mock_get_db_version):
+    mock_get_db_version.return_value = "foo"
+    response = client.get("/healthcheck")
+    assert response.status_code == 200
+    expected_val = {"database": {"able_to_connect": True, "db_version": "foo"}}
+    assert response.json() == expected_val
+
+
+@patch("api_gateway.api.get_db_version")
+@patch("api_gateway.api.log")
+def test_healthcheck_failure(mock_log, mock_get_db_version):
+    mock_get_db_version.side_effect = SQLAlchemyError()
+    response = client.get("/healthcheck")
+    assert response.status_code == 200
+    expected_val = {"database": {"able_to_connect": False}}
+    assert response.json() == expected_val
+    # assert mock_log.error.assert_called_once_with(SQLAlchemyError())
