b87bc9067c77d1644abdbb076d32f0dd22cee678
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-08-03 11:32:15 -0400

    feat: add a sg to the lambda, fix docker issue
    - Adds a security group to the lambda and connects it to the rds sg
    - Add a missing library to the image
    
    **Please Note** This TF change has already been applied

--- terragrunt/aws/api/vpc.tf
+++ terragrunt/aws/api/vpc.tf
@@ -19,3 +19,25 @@ resource "aws_security_group" "rds" {
     create_before_destroy = true
   }
 }
+
+resource "aws_security_group" "api_lambda" {
+  description = "Security group that is to be attached to the api lambda"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "api_lambda_sec_group"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "api_to_rds" {
+  type              = "ingress"
+  from_port         = 5432
+  to_port           = 5432
+  protocol          = "tcp"
+  security_group_id = aws_security_group.api_lambda.id
+  source_security_group_id = aws_security_group.rds.id
+}
