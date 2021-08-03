7e0df2d95c3f20558e2d2d89a51c6b05ca736577
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-08-03 17:21:25 -0400

    feat: healthcheck now returns db version
    - Added tests
    - Added __init__.py to databases folder
    - Fixed reference to logger in test_main

--- api/api_gateway/api.py
+++ api/api_gateway/api.py
@@ -7,22 +7,25 @@ from logger import log
 
 app = FastAPI()
 
-
 @app.get("/version")
 async def version():
     return {"version": environ.get("GIT_SHA", "unknown")}
 
-
-def healthcheck_session(sessionmaker):
+def get_db_version(sessionmaker):
     session = sessionmaker()
-    return session.query(text("1")).from_statement(text("SELECT 1")).all()
 
+    query = "SELECT version_num FROM alembic_version"
+    full_name = session.execute(query).fetchone()[0]
+    return full_name
 
 @app.get("/healthcheck")
 async def healthcheck():
     try:
-        healthcheck_session(db_session)
-        db_status = {"able_to_connect": True}
+        full_name =  get_db_version(db_session)
+        db_status = {
+            "able_to_connect": True,
+            "db_version": full_name
+        }
     except SQLAlchemyError as err:
         log.error(err)
         db_status = {"able_to_connect": False}
