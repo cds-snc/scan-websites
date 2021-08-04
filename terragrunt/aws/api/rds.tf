834a00f511ad87668881eb11a8f5a8f6fb1156e8
Calvin Rodo <calvin.rodo@cds-snc.ca>
2021-08-04 16:11:31 -0400

    feat/track db target group (#38)
    * feat: upgrad to the RDS version that has a target group
    
    * fix: upgrade to v0.0.20

--- terragrunt/aws/api/rds.tf
+++ terragrunt/aws/api/rds.tf
@@ -1,5 +1,5 @@
 module "rds" {
-  source                  = "github.com/cds-snc/terraform-modules?ref=v0.0.14//rds"
+  source                  = "github.com/cds-snc/terraform-modules?ref=v0.0.20//rds"
   backup_retention_period = 1
   billing_tag_value       = var.billing_code
   database_name           = "scan_websites"
