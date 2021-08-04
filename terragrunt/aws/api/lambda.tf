1a886f2eb750b79cff2cdf14c362166e8d16572f
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-08-04 16:27:29 -0400

    feat: attach lambda to vpc

--- terragrunt/aws/api/lambda.tf
+++ terragrunt/aws/api/lambda.tf
@@ -4,6 +4,7 @@ resource "aws_lambda_function" "api" {
   package_type = "Image"
   image_uri    = "${aws_ecr_repository.api.repository_url}:latest"
 
+  role = aws_iam_role.api.arn
   timeout = 60
 
   memory_size = 1024
@@ -14,7 +15,10 @@ resource "aws_lambda_function" "api" {
     }
   }
 
-  role = aws_iam_role.api.arn
+  vpc_config {
+    security_group_ids = [aws_security_group.api_lambda.id]
+    subnet_ids = module.vps.subnet_ids
+  }
 
   lifecycle {
     ignore_changes = [
@@ -29,4 +33,34 @@ resource "aws_lambda_permission" "api" {
   function_name = aws_lambda_function.api.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
+
+resource "aws_iam_role" "api" {
+  name               = "api_lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.service_principal_api.json
+}
+
+data "aws_iam_policy_document" "service_principal_api" {
+  statement {
+    effect = "Allow"
+
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+
+# Use AWS managed IAM policy
+####
+# Provides minimum permissions for a Lambda function to execute while 
+# accessing a resource within a VPC - create, describe, delete network 
+# interfaces and write permissions to CloudWatch Logs.
+####
+resource "aws_iam_role_policy_attachment" "AWSLambdaVPCAccessExecutionRole" {
+  role       = aws_iam_role.api.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
