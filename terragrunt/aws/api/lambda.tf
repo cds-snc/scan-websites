0575498e562aeba1a683da327bfa9bbbf9b46195
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-08-04 16:37:18 -0400

    fix: remove duplicate role

--- terragrunt/aws/api/lambda.tf
+++ terragrunt/aws/api/lambda.tf
@@ -34,33 +34,3 @@ resource "aws_lambda_permission" "api" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 }
-
-resource "aws_iam_role" "api" {
-  name               = "api_lambda_role"
-  assume_role_policy = data.aws_iam_policy_document.service_principal_api.json
-}
-
-data "aws_iam_policy_document" "service_principal_api" {
-  statement {
-    effect = "Allow"
-
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
-}
-
-
-# Use AWS managed IAM policy
-####
-# Provides minimum permissions for a Lambda function to execute while 
-# accessing a resource within a VPC - create, describe, delete network 
-# interfaces and write permissions to CloudWatch Logs.
-####
-resource "aws_iam_role_policy_attachment" "AWSLambdaVPCAccessExecutionRole" {
-  role       = aws_iam_role.api.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-}
