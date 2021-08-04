2fe55e409523f4973025ef5b1ddd84b1a06a792b
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-08-03 23:04:09 -0400

    feat: add a database connection timeout
    Postgresql connect_args documentation is here: https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-PARAMKEYWORDS

--- api/database/db.py
+++ api/database/db.py
@@ -3,5 +3,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_engine = create_engine(os.environ.get("SQLALCHEMY_DATABASE_URI"))
+connection_string = os.environ.get("SQLALCHEMY_DATABASE_URI")
+db_engine = create_engine(connection_string, connect_args={"connect_timeout": 10})
 db_session = sessionmaker(bind=db_engine)
