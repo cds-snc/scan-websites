9dbb4329a56ee4ddf66a2bc56c2dfe99e28ee650
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-08-04 21:44:40 -0400

    fix: use module provided sg for lambda

--- terragrunt/aws/api/vpc.tf
+++ terragrunt/aws/api/vpc.tf
@@ -5,24 +5,3 @@ module "vpc" {
   high_availability = true
   enable_flow_log   = false
 }
-
-
-resource "aws_security_group" "rds_lambda" {
-  description = "Security group that is to be attached to the RDS module and lambda"
-  vpc_id      = module.vpc.vpc_id
-
-  tags = {
-    Name = "rds_sec_group"
-  }
-
-  ingress {
-    from_port = 5432
-    to_port   = 5432
-    protocol  = "tcp"
-    self      = true
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
