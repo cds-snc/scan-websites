29db3bdbceb4c32a58bf0754ef950b367bf78778
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-08-04 21:49:02 -0400

    fix: update refernce to module

--- terragrunt/aws/api/rds.tf
+++ terragrunt/aws/api/rds.tf
@@ -1,5 +1,5 @@
 module "rds" {
-  source                  = "github.com/cds-snc/terraform-modules?ref=v0.0.24//rds"
+  source                  = "github.com/cds-snc/terraform-modules?ref=v0.0.25//rds"
   backup_retention_period = 1
   billing_tag_value       = var.billing_code
   database_name           = "scan_websites"
