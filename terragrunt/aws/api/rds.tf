b9e844f2cd4364e43f691458039f983317d874ff
Calvin Rodo <calvin.rodo@cds-snc.ca>
2021-08-04 21:51:06 -0400

    feat: refactor security groups
    - Removes old security groups
    - Adds the proxy and the labmda to the same sg
    - Upgrade the modules to the latest version
    - expose a security group from the module

--- terragrunt/aws/api/rds.tf
+++ terragrunt/aws/api/rds.tf
@@ -1,12 +1,11 @@
 module "rds" {
-  source                  = "github.com/cds-snc/terraform-modules?ref=v0.0.20//rds"
+  source                  = "github.com/cds-snc/terraform-modules?ref=v0.0.25//rds"
   backup_retention_period = 1
   billing_tag_value       = var.billing_code
   database_name           = "scan_websites"
   instances               = 1
   name                    = "scan-websites"
   preferred_backup_window = "07:00-09:00"
-  sg_ids                  = [aws_security_group.rds.id]
   subnet_ids              = module.vpc.private_subnet_ids
   username                = var.rds_username
   password                = var.rds_password
