d71c874c053723748bdc88b67f2d44b44695df76
dan sinclair <dan.sinclair@cds-snc.ca>
2021-08-06 13:20:21 -0400

    Use targets instead of for loop in top level makefile. (#47)
    This PR updates the top level makefile to generate targets for each of
    the [resource, command] pairs. The commands then invoke the relevant
    targets for each resources. This allows make to correctly handle an error
    in one of the targets and exit.
    
    Fixes #45

--- api/main.py
+++ api/main.py
@@ -16,7 +16,7 @@ def handler(event, context):
         response = asgi_handler(event, context)
         return response
 
-    elif event.get("task","") == "migrate":
+    elif event.get("task", "") == "migrate":
         try:
             migrate_head()
             return "Success"
