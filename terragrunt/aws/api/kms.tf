1b0178859a3e210d025564ae95de140879713fb5
Max Neuvians <max.neuvians@cds-snc.ca>
2021-08-10 15:11:34 -0400

    feat: add S3 bucket for axe core reports and SNS queue for axe-core-urls '

---
+++ terragrunt/aws/api/kms.tf
@@ -0,0 +1,53 @@
+  
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key" "scan-websites" {
+  description         = "KMS Key"
+  enable_key_rotation = true
+
+  policy = <<EOF
+{
+  "Version" : "2012-10-17",
+  "Id" : "key-default-1",
+  "Statement" : [ {
+      "Sid" : "Enable IAM User Permissions",
+      "Effect" : "Allow",
+      "Principal" : {
+        "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      },
+      "Action" : "kms:*",
+      "Resource" : "*"
+    },
+    {
+      "Effect": "Allow",
+      "Principal": { "Service": "logs.ca-central-1.amazonaws.com" },
+      "Action": [ 
+        "kms:Encrypt*",
+        "kms:Decrypt*",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:Describe*"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "Allow_CloudWatch_for_CMK",
+      "Effect": "Allow",
+      "Principal": {
+          "Service":[
+              "cloudwatch.amazonaws.com"
+          ]
+      },
+      "Action": [
+          "kms:Decrypt","kms:GenerateDataKey"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+
+  tags = {
+    CostCenter = var.billing_code
+  }
+}
