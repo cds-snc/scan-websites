2cbc32041ad5f65c0e8b6b883d590527639ece92
Max Neuvians <max.neuvians@cds-snc.ca>
2021-08-10 15:24:23 -0400

    fix: key name

--- terragrunt/aws/api/sns.tf
+++ terragrunt/aws/api/sns.tf
@@ -1,6 +1,6 @@
 resource "aws_sns_topic" "axe-core-urls" {
   name              = "axe-core-urls"
-  kms_master_key_id = aws_kms_key.a11y-tools.arn
+  kms_master_key_id = aws_kms_key.scan-websites.arn
 
   tags = {
     CostCenter = var.billing_code
