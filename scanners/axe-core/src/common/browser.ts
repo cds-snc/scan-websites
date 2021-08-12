c3f4c5b23c4f72a93dccb02f4e3f59d144d3d1e0
Max Neuvians <max.neuvians@cds-snc.ca>
2021-08-12 19:34:41 +0000

    feat: add CD for axe-core-scanner and fix some code issues

--- scanners/axe-core/src/common/browser.ts
+++ scanners/axe-core/src/common/browser.ts
@@ -1,10 +1,12 @@
-import { HTTPResponse, ScreenshotOptions, WaitForOptions } from "puppeteer";
+import { EvaluateFn, HTTPResponse, ScreenshotOptions, Viewport, WaitForOptions } from "puppeteer";
 
 export type Page = {
+  evaluate(F: EvaluateFn): Promise<number>;
   goto(S: string, O?: WaitForOptions): Promise<HTTPResponse | void>;
   screenshot(O?: ScreenshotOptions): Promise<string | void | Buffer>;
   setBypassCSP(B: boolean): Promise<void>;
   setContent(S: string, O?: WaitForOptions): Promise<void>;
+  setViewport(O?: Viewport): Promise<void>;
   setUserAgent(S: string): Promise<void>;
 };
 
--- scanners/axe-core/src/index.ts
+++ scanners/axe-core/src/index.ts
@@ -6,8 +6,46 @@ import { Impl, convertEventToRecords } from "./impl";
 const s3 = new S3();
 
 export const handler = async (event: SNSEvent | S3Event): Promise<boolean> => {
+
+  const options = [
+    '--allow-running-insecure-content',
+    '--autoplay-policy=user-gesture-required',
+    '--disable-component-update',
+    '--disable-domain-reliability',
+    '--disable-features=AudioServiceOutOfProcess,IsolateOrigins,site-per-process',
+    '--disable-print-preview',
+    '--disable-setuid-sandbox',
+    '--disable-site-isolation-trials',
+    '--disable-speech-api',
+    '--disable-web-security',
+    '--disk-cache-size=33554432',
+    '--enable-features=SharedArrayBuffer',
+    '--hide-scrollbars',
+    '--ignore-gpu-blocklist',
+    '--in-process-gpu',
+    '--mute-audio',
+    '--no-default-browser-check',
+    '--no-pings',
+    '--no-sandbox',
+    '--no-zygote',
+    '--use-gl=swiftshader',
+    '--window-size=1920,1080',
+    '--single-process'
+  ];
+
+  const viewport = {
+    deviceScaleFactor: 1,
+    hasTouch: false,
+    height: 1080,
+    isLandscape: true,
+    isMobile: false,
+    width: 1920,
+  };
+
   const browser = await puppeteer.launch({
-    args: ["--disable-dev-shm-usage", "--no-sandbox"],
+    args: options,
+    defaultViewport: viewport,
+    headless: true,
     ignoreHTTPSErrors: true,
   });
 
@@ -15,5 +53,5 @@ export const handler = async (event: SNSEvent | S3Event): Promise<boolean> => {
   const screenshotBucket = process.env.SCREENSHOT_BUCKET;
 
   const records = await convertEventToRecords(event, s3);
-  return Impl(records, browser, s3, reportBucket, screenshotBucket);
+  return await Impl(records, browser, s3, reportBucket, screenshotBucket);
 };
