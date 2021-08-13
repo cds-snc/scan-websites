8273e55d788391cbea8466b56cc1dc73ce1c7e25
Mohamed Nur <mohamed.nur@cds-snc.ca>
2021-08-12 21:34:13 -0400

    chore: remove encryption from ecr until project kms arn is exported

--- terragrunt/aws/scanners/owasp-zap/ecr.tf
+++ terragrunt/aws/scanners/owasp-zap/ecr.tf
@@ -7,8 +7,4 @@ resource "aws_ecr_repository" "scanners-owasp-zap" {
   image_scanning_configuration {
     scan_on_push = true
   }
-
-  encryption_configuration {
-    encryption_type = "KMS"
-  }
 }
