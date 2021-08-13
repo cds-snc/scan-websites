5f8da973ca5481c47f44fca5296afe64d623ffcd
Max Neuvians <max@neuvians.net>
2021-08-13 11:26:38 -0400

    feat: adds s3 expiry rules (#67)
    * feat: adds s3 expiry rules
    
    * fix: acl

--- terragrunt/aws/api/s3.tf
+++ terragrunt/aws/api/s3.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "axe-core-report-data" {
   bucket = "${var.product_name}-${var.env}-axe-core-report-data"
+  acl    = "private"
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
@@ -8,13 +9,32 @@ resource "aws_s3_bucket" "axe-core-report-data" {
     }
   }
 
+  lifecycle_rule {
+    id      = "expire"
+    enabled = true
+
+    expiration {
+      days = 30
+    }
+  }
+
   tags = {
     CostCenter = var.billing_code
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "axe-core-report-data" {
+  bucket = aws_s3_bucket.axe-core-report-data.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket" "axe-core-screenshots" {
   bucket = "${var.product_name}-${var.env}-axe-core-screenshots"
+  acl    = "private"
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
@@ -23,8 +43,25 @@ resource "aws_s3_bucket" "axe-core-screenshots" {
     }
   }
 
+  lifecycle_rule {
+    id      = "expire"
+    enabled = true
+
+    expiration {
+      days = 30
+    }
+  }
+
   tags = {
     CostCenter = var.billing_code
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "axe-core-screenshots" {
+  bucket = aws_s3_bucket.axe-core-screenshots.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
