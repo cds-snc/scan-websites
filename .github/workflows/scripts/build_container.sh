d9ee7151f12f3b0600013e4b29b30a118b035bf2
dan sinclair <dan.sinclair@cds-snc.ca>
2021-08-19 12:08:22 -0400

    fixes

--- .github/workflows/scripts/build_container.sh
+++ .github/workflows/scripts/build_container.sh
@@ -6,21 +6,23 @@ GITHUB_SHA=$1
 REGISTRY=$2
 IMAGE=$3
 
-EXTRA_ARGS=
+EXTRA_ARGS=""
 if [[ "$IMAGE" == "scanners/axe-core" ]];
 then
-  EXTRA_ARGS="$EXTRA_ARGS --build-arg CHROMIUM='with-chromium'"
+  EXTRA_ARGS="${EXTRA_ARGS} --build-arg CHROMIUM=with-chromium"
 fi
 
+DOCKER_FILE=Dockerfile
 if [[ $IMAGE == scanners/* ]];
 then
   cd ../..
-  EXTRA_ARGS="$EXTRA_ARGS --build-arg SCANNER_NAME='$IMAGE'"
+  DOCKER_FILE=scanners/Dockerfile
 fi
 
-docker build \
-    -f scanners/Dockerfile \
-    -t "$REGISTRY/$IMAGE:latest" \
-    --build-arg git_sha="$GITHUB_SHA" \
+docker --debug build \
+    -f $DOCKER_FILE \
+    -t "${REGISTRY}/${IMAGE}:latest" \
+    --build-arg IMAGE_NAME=${IMAGE} \
+    --build-arg git_sha=${GITHUB_SHA} \
     $EXTRA_ARGS \
     .
