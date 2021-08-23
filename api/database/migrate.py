8eb29f4c927c1777ba56f58a20353ee0985a1445
Max Neuvians <max@neuvians.net>
2021-08-23 13:25:56 -0400

    feat: sql tool and make migrations fix (#95)
    * feat: sql tool and make migrations fix
    
    * fix: tests

--- api/database/migrate.py
+++ api/database/migrate.py
@@ -4,4 +4,5 @@ from alembic import command
 
 def migrate_head():
     alembic_cfg = Config("./db_migrations/alembic.ini")
+    alembic_cfg.set_main_option("script_location", "./db_migrations")
     command.upgrade(alembic_cfg, "head")
