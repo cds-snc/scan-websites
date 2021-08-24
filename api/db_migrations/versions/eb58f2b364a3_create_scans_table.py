9a4fbc9d2bc76dfb5b5f821f5237fe3f52d9ebad
Max Neuvians <max.neuvians@cds-snc.ca>
2021-08-24 19:24:59 +0000

    fix: add organisation fk

--- api/db_migrations/versions/eb58f2b364a3_create_scans_table.py
+++ api/db_migrations/versions/eb58f2b364a3_create_scans_table.py
@@ -26,6 +26,10 @@ def upgrade():
         sa.Column("created_at", sa.DateTime, default=sa.func.utc_timestamp()),
         sa.Column("updated_at", sa.DateTime, onupdate=sa.func.utc_timestamp()),
         sa.ForeignKeyConstraint(
+            ["organisation_id"],
+            ["organisations.id"],
+        ),
+        sa.ForeignKeyConstraint(
             ["template_id"],
             ["templates.id"],
         ),
