a29c72e8951125f3979320562fb2d6267e0b798b
Mohamed Nur <mohamed.nur@cds-snc.ca>
2021-08-26 13:24:45 -0400

    chore: test removal of db initialize

--- api/database/db.py
+++ api/database/db.py
@@ -8,4 +8,4 @@ connection_string = os.environ.get("SQLALCHEMY_DATABASE_URI")
 # Timeout is set to 10 seconds
 db_engine = create_engine(connection_string, connect_args={"connect_timeout": 10})
 db_session = sessionmaker(bind=db_engine)
-initialize_sql(db_engine)
+# initialize_sql(db_engine)
