3e82360aef2d4d83eb57d7b435f215529b086de0
Mohamed Nur <mohamed.nur@cds-snc.ca>
2021-08-26 13:26:49 -0400

    chore: remove import of disabled feature

--- api/database/db.py
+++ api/database/db.py
@@ -2,7 +2,8 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import initialize_sql
+
+# from models import initialize_sql
 
 connection_string = os.environ.get("SQLALCHEMY_DATABASE_URI")
 # Timeout is set to 10 seconds
