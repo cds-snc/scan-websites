a76294f8f3a1c0f70b931cd18681a4328c093fd7
Mohamed Nur <mohamed.nur@cds-snc.ca>
2021-09-03 16:18:07 -0400

    feat: crawl in the background

--- api/api_gateway/routers/scans.py
+++ api/api_gateway/routers/scans.py
@@ -14,7 +14,6 @@ class CrawlUrl(BaseModel):
 @router.post("/crawl")
 def crawl_endpoint(crawl_url: CrawlUrl, background_tasks: BackgroundTasks):
     log.info(f"Crawling {crawl_url}")
-    # background_tasks.add_task(uuid.uuid4(), crawl_url.url)
-    crawl(str(uuid.uuid4()), crawl_url.url)
-    return {"message": "Crawler initiated"}
+    background_tasks.add_task(str(uuid.uuid4()), crawl_url.url)
+    return {"message": "Crawler background task initiated"}
 
