21b23de1ce72851bd28800b961901705507619a4
Mohamed Nur <85885638+mohamed-cds@users.noreply.github.com>
2021-09-07 14:27:35 -0400

    Feat/crawler ratelimit (#109)
    * feat: rate limit crawler to 5 requests a minute per ip
    
    * chore: formatting

--- api/api_gateway/routers/scans.py
+++ api/api_gateway/routers/scans.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, BackgroundTasks
+from fastapi import APIRouter, BackgroundTasks, Request
 from logger import log
 from pydantic import BaseModel
 from crawler.crawler import crawl
+from slowapi import Limiter
+from slowapi.util import get_remote_address
 import uuid
 
+limiter = Limiter(key_func=get_remote_address, enabled=True)
 router = APIRouter()
 
 
@@ -12,7 +15,10 @@ class CrawlUrl(BaseModel):
 
 
 @router.post("/crawl")
-def crawl_endpoint(crawl_url: CrawlUrl, background_tasks: BackgroundTasks):
+@limiter.limit("5/minute")
+def crawl_endpoint(
+    crawl_url: CrawlUrl, background_tasks: BackgroundTasks, request: Request
+):
     log.info(f"Crawling {crawl_url}")
     background_tasks.add_task(crawl, str(uuid.uuid4()), crawl_url.url)
     return {"message": "Crawler initiated"}
