d7f04d4f4542136b493b2e6727c0ccdf2b442d6d
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-09-08 12:17:16 -0400

    feat: use entr to watch files for scanner

--- scanners/axe-core/bin/entry.sh
+++ scanners/axe-core/bin/entry.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
-    exec /usr/bin/aws-lambda-rie /usr/local/bin/npx aws-lambda-ric $1
+    # Watch all javascript, typescript and json files and reload if any of them change
+    exec find . -type f \( -name "*.json" -o -name "*.js" -o -name "*.ts" \) -o -path ./node_modules -prune -false  \
+        | entr -r /usr/bin/aws-lambda-rie /usr/local/bin/npx aws-lambda-ric "$1"
 else
-    exec /usr/local/bin/npx aws-lambda-ric $1
+    exec /usr/local/bin/npx aws-lambda-ric "$1"
 fi
