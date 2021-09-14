b253e73369ab162e609e9dd8db7777bf366ffed6
Max Neuvians <max.neuvians@cds-snc.ca>
2021-09-14 13:26:43 +0000

    fix: hold

--- api/storage/storage.py
+++ api/storage/storage.py
@@ -1,4 +1,3 @@
-import functools
 import json
 
 from database.db import db_session
@@ -27,7 +26,7 @@ def get_object(record):
 
 
 def store(record):
-    session = db_session()
+    name = record.s3.bucket.name
     body = get_object(record)
 
     if not body:
@@ -35,38 +34,44 @@ def store(record):
 
     try:
         payload = json.loads(body)
-        scan = (
-            session.query(Scan).options(joinedload(Scan.scan_type)).get(payload["id"])
-        )
-        session.close()
-        if scan is None:
-            log.error(f"Scan ID: {payload['id']} could not be found")
-            return False
 
-        if scan.scan_type.name == "axe_core":
-            return store_axe_core_record(scan, payload)
+        if name == "axe_core":
+            return store_axe_core_record(payload)
         else:
-            log.error(f"Error storing {scan.id} with type {scan.scan_type.name}")
+            log.error(f"Unknown bucket {name}")
             return False
 
     except Exception:
-        log.error(f"Error decoding {record.s3.object.key} from {record.s3.bucket.name}")
+        log.error(f"Error decoding {record.s3.object.key} from {name}")
         return False
 
 
-def store_axe_core_record(scan, payload):
+def store_axe_core_record(payload):
     session = db_session()
     a11y_report = session.query(A11yReport).get(payload["id"])
-    report = payload["report"]
-    summary = {
-        "status": "completed",
-        "inapplicable": len(report["inapplicable"]),
-        "incomplete": len(report["incomplete"]),
-        "violations": functools.reduce(
-            lambda d, c: d.update({c["impact"] : 1}), report["violations"], {}
-        ),
-        "passes": len(report["passes"])
-    }
-    # summary["violations"]["total"] = sum(list(summary["violations"].values()))
-    print(summary)
-    return True
+    if a11y_report is not None:
+        report = payload["report"]
+        summary = {
+            "status": "completed",
+            "inapplicable": len(report["inapplicable"]),
+            "incomplete": len(report["incomplete"]),
+            "violations": sum_impact(report["violations"]),
+            "passes": len(report["passes"]),
+        }
+        summary["violations"]["total"] = sum(list(summary["violations"].values()))
+        a11y_report.summary = summary
+        session.commit()
+        return True
+    else:
+        return False
+
+
+def sum_impact(violations):
+    d = {}
+    for v in violations:
+        if "impact" in v:
+            if v["impact"] in d:
+                d[v["impact"]] = d[v["impact"]] + 1
+            else:
+                d[v["impact"]] = 1
+    return d
--- api/tests/storage/test_storage.py
+++ api/tests/storage/test_storage.py
@@ -9,9 +9,9 @@ def load_fixture(name):
     return fixture.read()
 
 
-def mock_record():
+def mock_record(name="bucket_name"):
     mock_record = MagicMock()
-    mock_record.s3.bucket.name = "bucket_name"
+    mock_record.s3.bucket.name = name
     mock_record.s3.object.key = "key"
     return mock_record
 
@@ -62,52 +62,46 @@ def test_store_with_not_json(mock_get_object, mock_log):
 def test_store_with_empty_json(mock_get_object, mock_log):
     mock_get_object.return_value = load_fixture("empty.json")
     assert storage.store(mock_record()) is False
-    mock_log.error.assert_called_once_with("Error decoding key from bucket_name")
+    mock_log.error.assert_called_once_with("Unknown bucket bucket_name")
 
 
-@patch("storage.storage.db_session")
 @patch("storage.storage.log")
 @patch("storage.storage.get_object")
-def test_store_with_scan_missing(mock_get_object, mock_log, mock_session):
-    mock_session().query().options().get.return_value = None
+def test_store_with_unknown_bucket(mock_get_object, mock_log):
     mock_get_object.return_value = load_fixture("axe_core_report.json")
-    assert storage.store(mock_record()) is False
-    mock_log.error.assert_called_once_with("Scan ID: scan_id could not be found")
+    assert storage.store(mock_record("unknown")) is False
+    mock_log.error.assert_called_once_with("Unknown bucket unknown")
 
 
-@patch("storage.storage.db_session")
-@patch("storage.storage.log")
-@patch("storage.storage.get_object")
-def test_store_with_scan_type_missing(mock_get_object, mock_log, mock_session):
-    mock_scan = MagicMock()
-    mock_scan.id = "scan_id"
-    mock_scan.scan_type.name = "unknown_type_name"
-    mock_session().query().options().get.return_value = mock_scan
-    mock_get_object.return_value = load_fixture("axe_core_report.json")
-    assert storage.store(mock_record()) is False
-    mock_log.error.assert_called_once_with(
-        "Error storing scan_id with type unknown_type_name"
-    )
-
-
-@patch("storage.storage.db_session")
-@patch("storage.storage.log")
 @patch("storage.storage.get_object")
 @patch("storage.storage.store_axe_core_record")
-def test_store_with_scan_type_as_axe_core(
-    mock_store_axe_core, mock_get_object, mock_log, mock_session
-):
-    mock_scan = MagicMock()
-    mock_scan.id = "scan_id"
-    mock_scan.scan_type.name = "axe_core"
+def test_store_with_bucket_type_as_axe_core(mock_store_axe_core, mock_get_object):
     mock_store_axe_core.return_value = True
-    mock_session().query().options().get.return_value = mock_scan
     mock_get_object.return_value = load_fixture("axe_core_report.json")
-    assert storage.store(mock_record()) is True
+    assert storage.store(mock_record("axe_core")) is True
     mock_store_axe_core.assert_called_once()
 
 
 @patch("storage.storage.db_session")
-def test_store_axe_core_record(_mock_session):
+def test_store_axe_core_record_returns_false_on_missing_record(mock_session):
+    mock_session().query().get.return_value = None
     payload = json.loads(load_fixture("axe_core_report.json"))
-    storage.store_axe_core_record({}, payload)
+    assert storage.store_axe_core_record(payload) == False
+
+
+def test_store_axe_core_record_updates_record(session, a11y_report_fixture):
+    payload = json.loads(load_fixture("axe_core_report.json"))
+    payload["id"] = a11y_report_fixture.id
+    assert storage.store_axe_core_record(payload) == True
+
+
+def test_sum_impact_empty():
+    assert storage.sum_impact([]) == {}
+
+
+def test_sum_impact_missing_impact():
+    assert storage.sum_impact([{"foo": "bar"}]) == {}
+
+
+def test_sum_impact_correct_impact():
+    assert storage.sum_impact([{"impact": "impact_value"}]) == {"impact_value": 1}
