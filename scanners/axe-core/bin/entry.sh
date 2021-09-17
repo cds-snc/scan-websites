f047b08323bb77a5d7983ec95621f288f8d427f3
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-09-17 11:34:31 -0400

    chore: commiting changes that probably odn't work

--- scanners/axe-core/bin/entry.sh
+++ scanners/axe-core/bin/entry.sh
@@ -3,6 +3,10 @@ if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
 
     # exec /usr/bin/aws-lambda-rie /usr/local/bin/npx aws-lambda-ric "$1"
     # Watch all javascript, typescript and json files and reload if any of them change
+    apk add cmake cmake-doc extra-cmake-modules extra-cmake-modules-doc make
+    cd /function || exit
+    make install-dev
+    
     exec /local.sh "$1"
 else
     exec /usr/local/bin/npx aws-lambda-ric "$1"
