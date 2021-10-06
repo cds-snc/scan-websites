181e0b696b6ad18a3b26b29e96312437ee7d3536
Mohamed Nur <mohamed.nur@cds-snc.ca>
2021-10-06 16:29:19 -0400

    chore: locale and js cleanup

--- api/front_end/static/js/template.js
+++ api/front_end/static/js/template.js
@@ -1,15 +1,8 @@
 $(document).ready(function() {
-  $("#deleteScan a").click(function(event) {
+  $(document).on("click", "#deleteScan a", function(event) {
     event.preventDefault();
     event.stopPropagation();
-      // Jinja2 location not accessible, so translations moved here
-      var text = "Are you sure you want to delete this scan ?"
-
-      if (window.location.href.indexOf("/fr/") > -1) {
-        text = "Etes-vous sûr de vouloir supprimer ce scan ?"
-      }
-
-      if (confirm(text)) {
+      if (confirm($(this).attr('data-confirm'))) {
         $.ajax({
           url:this.href,
           type:"DELETE",
