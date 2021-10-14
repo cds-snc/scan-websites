d90a69d484e30b3c2b1a0670fa1a0786abd24860
Mohamed Nur <85885638+mohamed-cds@users.noreply.github.com>
2021-10-14 13:11:00 -0400

    fix: scan table correct headers (#169)
    * fix: scan table correct headers
    
    * fix: missing code
    
    * feat: bump hsts to 1 week to continue testing
    
    * fix: html tag nesting

--- api/api_gateway/custom_middleware.py
+++ api/api_gateway/custom_middleware.py
@@ -3,5 +3,5 @@ from fastapi import Request
 
 async def add_security_headers(request: Request, call_next):
     response = await call_next(request)
-    response.headers["Strict-Transport-Security"] = "max-age=300 ; includeSubDomains"
+    response.headers["Strict-Transport-Security"] = "max-age=604800 ; includeSubDomains"
     return response
