23aea01678a6a00cb0572a5d3841db48b947eaed
Mohamed Nur <85885638+mohamed-cds@users.noreply.github.com>
2021-10-15 14:08:46 -0400

    feat: vulnerability details client side views (#181)
    * feat: vulnerability details client side views
    
    * chore: typo
    
    * fix: a11y issues and cleanup

--- api/tests/front_end/test_security_report_view.py
+++ api/tests/front_end/test_security_report_view.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 
 from api_gateway import api
+from front_end.view import get_risk_colour
 
 client = TestClient(api.app)
 
@@ -58,4 +59,29 @@ def test_security_report_violations(
         f"/en/results/{home_org_owasp_zap_template_fixture.id}/security/{home_org_owasp_scan_fixture.id}/{owasp_security_report_fixture.id}"
     )
     assert response.status_code == 200
+    assert response.template.name == "scan_results_security_summary.html"
+
+
+def test_security_report_violation_details(
+    home_org_owasp_scan_fixture,
+    home_org_owasp_zap_template_fixture,
+    owasp_security_report_fixture,
+    owasp_security_violation_fixture,
+    logged_in_client,
+):
+    response = logged_in_client.get(
+        f"/en/results/{home_org_owasp_zap_template_fixture.id}/security/{home_org_owasp_scan_fixture.id}/{owasp_security_report_fixture.id}/{owasp_security_violation_fixture.id}"
+    )
+    assert response.status_code == 200
     assert response.template.name == "scan_results_security_details.html"
+
+
+def risk_colour_assignment_valid():
+    assert get_risk_colour("0") == "bg-blue-500"
+    assert get_risk_colour("1") == "bg-green-500"
+    assert get_risk_colour("2") == "bg-orange-500"
+    assert get_risk_colour("3") == "bg-red-500"
+
+
+def risk_colour_assignment_invalid_defaults_gray():
+    assert get_risk_colour("foo") == "bg-gray-500"
