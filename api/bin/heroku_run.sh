dfee7b81a7821a7d31e2d46a40753be2243d2b32
Max Neuvians <max.neuvians@cds-snc.ca>
2021-10-21 09:42:40 -0400

    fix: paths

--- api/bin/heroku_run.sh
+++ api/bin/heroku_run.sh
@@ -2,5 +2,5 @@
 cd /function/db_migrations
 SQLALCHEMY_DATABASE_URI=$DATABASE_URL /pymodules/bin/alembic upgrade head
 cd ..
-SQLALCHEMY_DATABASE_URI=$DATABASE_URL /usr/local/bin/python database/seed.py
-SQLALCHEMY_DATABASE_URI=$DATABASE_URL /usr/local/bin/uvicorn main:app --port $PORT
+SQLALCHEMY_DATABASE_URI=$DATABASE_URL python database/seed.py
+SQLALCHEMY_DATABASE_URI=$DATABASE_URL python -m uvicorn main:app --port $PORT
