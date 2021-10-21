8a074489e49905d24bac779660fa7b3972b757a5
Max Neuvians <max.neuvians@cds-snc.ca>
2021-10-20 21:51:51 -0400

    fix: small tweaks

--- api/bin/heroku_run.sh
+++ api/bin/heroku_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /function
-make migrations
-make seed
-uvicorn main:app --port $PORT
+SQLALCHEMY_DATABASE_URI=$DATABASE_URL make migrations
+SQLALCHEMY_DATABASE_URI=$DATABASE_URL make seed
+SQLALCHEMY_DATABASE_URI=$DATABASE_URL uvicorn main:app --port $PORT
