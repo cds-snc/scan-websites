1b6bfbeee1ffcc689e9bc38aa128e426be253809
Max Neuvians <max.neuvians@cds-snc.ca>
2021-10-20 20:43:59 -0400

    fix: DB ENV

--- api/bin/heroku_run.sh
+++ api/bin/heroku_run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd .. &&\
-make install &&\
-SQLALCHEMY_DATABASE_URI=$DATABASE_URL make migrations &&\
-SQLALCHEMY_DATABASE_URI=$DATABASE_URL make seed &&\
-SQLALCHEMY_DATABASE_URI=$DATABASE_URL uvicorn main:app --port $PORT
+make migrations &&\
+make seed &&\
+uvicorn main:app --port $PORT
