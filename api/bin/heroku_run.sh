690164624327f584592041f78b0b8fafa6a2cf28
Max Neuvians <max.neuvians@cds-snc.ca>
2021-10-21 11:18:30 -0400

    fix: try forwarded IPs

--- api/bin/heroku_run.sh
+++ api/bin/heroku_run.sh
@@ -7,4 +7,4 @@ cd /function/db_migrations
 SQLALCHEMY_DATABASE_URI=$db /pymodules/bin/alembic upgrade head
 cd ..
 SQLALCHEMY_DATABASE_URI=$db python database/seed.py
-SQLALCHEMY_DATABASE_URI=$db python -m uvicorn main:app --host 0.0.0.0 --port $PORT --proxy-headers
+SQLALCHEMY_DATABASE_URI=$db python -m uvicorn main:app --host 0.0.0.0  --proxy-headers --forwarded-allow-ips="*"  --port $PORT
