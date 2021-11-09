14518782c6bf6948a3ea122867b60fb02c794f2e
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-11-09 14:59:30 -0500

    fix: remove unused input

--- terragrunt/aws/scanners/github/inputs.tf
+++ terragrunt/aws/scanners/github/inputs.tf
@@ -2,10 +2,6 @@ variable "github_report_data_bucket_id" {
   type = string
 }
 
-variable "github_screenshots_bucket_id" {
-  type = string
-}
-
 variable "github_urls_topic_arn" {
   type = string
 }
