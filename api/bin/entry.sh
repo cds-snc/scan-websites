d0742de8f3e8712f5bfc3f48256ca52464d91b3a
Mohamed Nur <mohamed.nur@cds-snc.ca>
2021-12-21 22:22:02 -0500

    feat: debian based opentelemetry autoinstrumentation

--- api/bin/entry.sh
+++ api/bin/entry.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
     echo "Running aws-lambda-rie"
     exec find . -name "*.py" | entr -r /usr/bin/aws-lambda-rie /usr/local/bin/python -m awslambdaric "$1"
 else
-    exec /usr/local/bin/python -m awslambdaric "$1"
+    if [ -z "$AWS_LAMBDA_EXEC_WRAPPER" ]; then
+        exec /usr/local/bin/python -m awslambdaric "$1"
+    else
+        # If Opentelemetry is enabled use the defined otel wrapper
+        exec -- "$AWS_LAMBDA_EXEC_WRAPPER" /usr/local/bin/python -m awslambdaric "$1"
+    fi
 fi
