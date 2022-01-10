979745a63d6bf9285d081e08fd567e1a571147c1
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-01-10 10:52:15 -0500

    feat: add roles for tf plan and apply using OICD
    - Adds a role restriced to main branch that can has the admin managed
      policy attached.
    
    - Adds a role that has no branch restriction that has the readonly
      managed policy attached

---
+++ terragrunt/aws/tf_roles/main.tf
@@ -0,0 +1,32 @@
+module "plan_role" {
+  source            = "github.com/cds-snc/terraform-modules?ref=v0.0.48//gh_oicd_role"
+  role_name         = "gh_plan_role"
+  repo              = "scan-websites"
+  billing_tag_value = var.billing_code
+}
+
+data "aws_iam_policy" "readonly" {
+  name = "ReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "readonly" {
+  role       = module.plan_role.role_name
+  policy_arn = data.aws_iam_policy.readonly.arn
+}
+
+module "apply_role" {
+  source            = "github.com/cds-snc/terraform-modules?ref=v0.0.48//gh_oicd_role"
+  role_namename     = "gh_plan_role"
+  repo              = "scan-websites"
+  claim             = "ref:refs/heads/main"
+  billing_tag_value = var.billing_code
+}
+
+data "aws_iam_policy" "admin" {
+  name = "AdministratorAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "admin" {
+  role       = module.apply_role.role_name
+  policy_arn = data.aws_iam_policy.admin.arn
+}
