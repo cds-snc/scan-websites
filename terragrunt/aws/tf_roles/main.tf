0e78496cef94f4ae38e927214a9ef680a920e349
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-01-11 13:09:02 -0500

    feat: update to v1.0.0 for gh_oidc_role module

--- terragrunt/aws/tf_roles/main.tf
+++ terragrunt/aws/tf_roles/main.tf
@@ -1,25 +1,35 @@
-module "plan_role" {
-  source            = "github.com/cds-snc/terraform-modules?ref=v0.0.48//gh_oicd_role"
-  role_name         = "gh_plan_role"
-  repo              = "scan-websites"
+locals {
+  plan_name = "gh_plan_role"
+  admin_name = "gh_admin_role"
+}
+
+module "gh_oidc_roles" {
+  source            = "github.com/cds-snc/terraform-modules?ref=v1.0.0//gh_oidc_role"
+  roles = [
+    {
+      name  = local.plan_name
+      repo  = "scan-websites"
+      claim = "*"
+    },
+    {
+      name  = local.admin_name
+      repo  = "scan-websites"
+      claim = "ref:refs/heads/main"
+    }
+  ]
+
   billing_tag_value = var.billing_code
 }
+
+
 
 data "aws_iam_policy" "readonly" {
   name = "ReadOnlyAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "readonly" {
-  role       = module.plan_role.role_name
+  role       = module.gh_oidc_roles[local.plan_name].name
   policy_arn = data.aws_iam_policy.readonly.arn
-}
-
-module "apply_role" {
-  source            = "github.com/cds-snc/terraform-modules?ref=v0.0.48//gh_oicd_role"
-  role_name         = "gh_plan_role"
-  repo              = "scan-websites"
-  claim             = "ref:refs/heads/main"
-  billing_tag_value = var.billing_code
 }
 
 data "aws_iam_policy" "admin" {
@@ -27,6 +37,6 @@ data "aws_iam_policy" "admin" {
 }
 
 resource "aws_iam_role_policy_attachment" "admin" {
-  role       = module.apply_role.role_name
+  role       = module.gh_oidc_roles[local.admin_name].name
   policy_arn = data.aws_iam_policy.admin.arn
 }
