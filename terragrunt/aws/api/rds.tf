8e3ed3a4a9ce602fe257f0043e2e0252a691bb0f
Max Neuvians <max.neuvians@cds-snc.ca>
2022-05-17 16:58:03 -0400

    chore: upgrade postgres to 13.4 (really now) (#318)

--- terragrunt/aws/api/rds.tf
+++ terragrunt/aws/api/rds.tf
@@ -1,14 +1,15 @@
 module "rds" {
-  source                  = "github.com/cds-snc/terraform-modules?ref=v0.0.27//rds"
-  backup_retention_period = 1
-  billing_tag_value       = var.billing_code
-  database_name           = "scan_websites"
-  instances               = 1
-  name                    = "scan-websites"
-  preferred_backup_window = "07:00-09:00"
-  subnet_ids              = module.vpc.private_subnet_ids
-  username                = var.rds_username
-  password                = var.rds_password
-  vpc_id                  = module.vpc.vpc_id
-  engine_version          = "13.4"
+  source                      = "github.com/cds-snc/terraform-modules?ref=v0.0.27//rds"
+  backup_retention_period     = 1
+  billing_tag_value           = var.billing_code
+  database_name               = "scan_websites"
+  instances                   = 1
+  name                        = "scan-websites"
+  preferred_backup_window     = "07:00-09:00"
+  subnet_ids                  = module.vpc.private_subnet_ids
+  username                    = var.rds_username
+  password                    = var.rds_password
+  vpc_id                      = module.vpc.vpc_id
+  engine_version              = "13.4"
+  allow_major_version_upgrade = true
 }
