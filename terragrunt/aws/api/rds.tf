989e0e08208d678daee5be31144655017a7b80b7
Max Neuvians <max.neuvians@cds-snc.ca>
2022-09-07 16:01:35 -0400

    fix: sync with clickops (#319)
    Update the Terraform to match the Aurora Serverless V2
    instance we created manually in the AWS console.
    
    Also removes the breakglass alarms that need to be updated
    to read from the new Control Tower CloudTrail logs.
    
    Co-authored-by: Pat Heard <patrick.heard@cds-snc.ca>

--- terragrunt/aws/api/rds.tf
+++ terragrunt/aws/api/rds.tf
@@ -1,15 +1,23 @@
 module "rds" {
-  source                      = "github.com/cds-snc/terraform-modules?ref=v0.0.27//rds"
-  backup_retention_period     = 1
-  billing_tag_value           = var.billing_code
-  database_name               = "scan_websites"
-  instances                   = 1
-  name                        = "scan-websites"
-  preferred_backup_window     = "07:00-09:00"
-  subnet_ids                  = module.vpc.private_subnet_ids
-  username                    = var.rds_username
-  password                    = var.rds_password
+  source = "github.com/cds-snc/terraform-modules?ref=v3.0.16//rds"
+
+  name           = "scan-websites"
+  database_name  = "scan_websites"
+  instances      = 1
+  instance_class = "db.serverless"
+  engine         = "aurora-postgresql"
+  engine_version = "13.6"
+  username       = var.rds_username
+  password       = var.rds_password
+
   vpc_id                      = module.vpc.vpc_id
-  engine_version              = "13.4"
+  subnet_ids                  = module.vpc.private_subnet_ids
+  preferred_backup_window     = "07:00-09:00"
+  backup_retention_period     = 1
   allow_major_version_upgrade = true
+
+  serverless_min_capacity = 0.5
+  serverless_max_capacity = 1.0
+
+  billing_tag_value = var.billing_code
 }
