4450a2c87de459e2a705c5b1df871cc9fafd743b
Pat Heard <patrick.heard@cds-snc.ca>
2022-09-07 19:51:54 +0000

    chore: remove breakglass alarms
    The breakglass alarm needs to be update to use the
    new Control Tower CloudTrail logs.

--- terragrunt/aws/alarms/cloudwatch_breakglass.tf
+++
@@ -1,9 +0,0 @@
-module "ops_alarms" {
-  source                = "github.com/cds-snc/terraform-modules?ref=v3.0.16//user_login_alarm"
-  account_names         = ["ops1", "ops2"]
-  namespace             = "ops_alarms"
-  log_group_name        = "CloudTrail/Landing-Zone-Logs"
-  alarm_actions_success = [var.sns_topic_critical_arn]
-  alarm_actions_failure = [var.sns_topic_warning_arn]
-  num_attempts          = 1
-}
