2caf908811649fe6fd6aeea0a707bfdab46fb58d
Pat Heard <patrick.heard@cds-snc.ca>
2022-09-20 18:36:28 -0400

    fix: remove OpenTelemetry from API lambda (#372)
    Update the API to no longer use OpenTelemetry.  This is because OTEL is
    volatile and has caused the API to break in the past and we aren't
    currently using it.

--- api/bin/entry.sh
+++ api/bin/entry.sh
@@ -3,10 +3,5 @@ if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
     echo "Running aws-lambda-rie"
     exec find . -name "*.py" | entr -r /usr/bin/aws-lambda-rie /usr/local/bin/python -m awslambdaric "$1"
 else
-    if [ -z "$AWS_LAMBDA_EXEC_WRAPPER" ]; then
-        exec /usr/local/bin/python -m awslambdaric "$1"
-    else
-        # If Opentelemetry is enabled use the defined otel wrapper
-        exec -- "$AWS_LAMBDA_EXEC_WRAPPER" /usr/local/bin/python -m awslambdaric "$1"
-    fi
+    exec /usr/local/bin/python -m awslambdaric "$1"
 fi
