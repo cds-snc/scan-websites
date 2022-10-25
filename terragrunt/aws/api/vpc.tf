fdc83b9642d0294886b16f34aa144e02415d73f6
Pat Heard <patrick.heard@cds-snc.ca>
2022-10-24 21:08:38 -0400

    chore: use single NAT gateway
    Switch to using a single NAT gateway to save money.

--- terragrunt/aws/api/vpc.tf
+++ terragrunt/aws/api/vpc.tf
@@ -1,9 +1,11 @@
 module "vpc" {
-  source            = "github.com/cds-snc/terraform-modules?ref=v0.0.21//vpc"
+  source            = "github.com/cds-snc/terraform-modules?ref=v3.0.19//vpc"
   name              = var.product_name
-  billing_tag_value = var.billing_code
   high_availability = true
   enable_flow_log   = false
+
+  single_nat_gateway = true
+  billing_tag_value  = var.billing_code  
 }
 
 resource "aws_network_acl_rule" "https" {
